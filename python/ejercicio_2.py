015128d5cb71eb461c8ff016a3991fe43dde0968
Carlos Aguirre <aguizandre.2803@gmail.com>
2023-02-01 10:12:10 -0500

    Nuevo ejercicio.

--- python/ejercicio_2.py
+++ python/ejercicio_2.py
@@ -1,3 +1,7 @@
+'''
+Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver
+la modificación de la variable.
+'''
 a = '¡Hola mundo!'
 print(a)
 a = '¡Hola mundo otra vez!'
--- python/ejercicio_4.py
+++ python/ejercicio_4.py
@@ -1,3 +1,6 @@
+'''
+Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
+'''
 i = 0
 while i <= 99:
     num = 100 - i
